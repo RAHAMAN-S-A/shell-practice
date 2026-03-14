913fdf7867da0fd91f132aaa8e806014394457a9
rahaman <you@example.com>
2026-03-14 15:00:42 +0530

    changed

--- 02.shell.sh
+++ 02.shell.sh
@@ -4,9 +4,7 @@ echo " hello "
 
 echo " hai "
 
-today=wednesday
-if(today != sunday){
-	print ("goto school")
-}
-else{
-	print("take holiday, enjoy")
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learning Ramesh?"
+echo "Ramesh:: I am learning Shell. What about you?"
