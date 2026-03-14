7dcf1a849053ffacd8f9b1b0cbf8ab04bb747643
rahaman <you@example.com>
2026-03-14 15:20:44 +0530

    changed2

--- 02.shell.sh
+++ 02.shell.sh
@@ -5,7 +5,16 @@ echo " hello "
 echo " hai "
 
 echo "Suresh:: Hello Ramesh, How are you?"
+
 echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
 echo "Suresh:: doing fine. What are you learning Ramesh?"
-echo "Ramesh:: I am learning Shell. What about you?"
+echo "Ramesh:: I am learning Shell. What about you
 
+
+PERSON1=Trump # no space between value and =
+PERSON2=Putin
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
