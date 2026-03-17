40cbdbd71b9532efcccef084c55d1ded620ea699
rahaman <you@example.com>
2026-03-17 14:31:23 +0530

    changes

--- 02.shell.sh
+++ 02.shell.sh
@@ -51,3 +51,9 @@ sleep 100 &
 echo "PID of recently executed background process: $!"
 echo "All args passed to script: $*"
 
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
