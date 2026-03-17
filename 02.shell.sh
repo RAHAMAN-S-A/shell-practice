bc271b775eb795b6199d4987cec29ec08f5e8bf0
rahaman <you@example.com>
2026-03-17 14:17:51 +0530

    changes

--- 02.shell.sh
+++ 02.shell.sh
@@ -28,3 +28,26 @@ echo "Please enter your password::"
 read -s PASSWORD
 
 echo "Password is $PASSWORD"
+
+echo "Please Enter your username::"
+read USER_NAME
+
+echo "User name is $USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
+
+echo "Password is $PASSWORD"
+
+
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "All args passed to script: $*"
+
