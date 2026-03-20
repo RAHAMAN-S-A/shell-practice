48670d4c49d64a2aa9eb6417cac38470eefe1b09
rahaman <you@example.com>
2026-03-20 13:16:17 +0530

    changes

--- real.sh
+++ real.sh
@@ -9,3 +9,17 @@ if [ $user_id -ne 0 ]; then
 
  fi 
 
+validate(){
+
+if [ $1 -ne 0 ]; then
+ echo " $2 is failed "
+ exit 1
+ else
+ echo " $2 is success "
+}
+
+dnf instal nginx -y
+validate $? " installing nginx "
+
+dng Instal nodejs -y
+validate $? " installing nodejs "
