23a1f09af5c1438ca05103f8768cc03d969eab66
Rahaman <your-email@example.com>
2026-04-25 16:50:04 +0530

    roles commit

--- 05.backup.sh
+++ 05.backup.sh
@@ -1,10 +1,14 @@
 #!/bash/bin/
 
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
 
 if [ $USERID -ne 0 ]; then
  echo "use sudo user"
  exit 1
 fi
+
+mkdir -p $LOGS_FOLDER
 
 
